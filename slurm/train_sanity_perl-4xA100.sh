9b9d8fb1dca23047701a643b884f15519e3a22b6
tjbai <tjianbai@gmail.com>
2025-05-04 13:12:36 -0700

    sanity run queued on small 100 subset

--- slurm/train_sanity_perl-4xA100.sh
+++ slurm/train_sanity_perl-4xA100.sh
@@ -1,5 +1,5 @@
-sbatch slurm/train_perl-4xA100.slurm \
-    --model DeepSeek-R1-Distill-Qwen-1.5B \
-    --config sanity \
-    --accelerator zero2 \
-    --task grpo \
+sbatch ./slurm/train_perl-4xA100.slurm \
+	--model DeepSeek-R1-Distill-Qwen-1.5B \
+	--config sanity \
+	--accelerator zero2 \
+	--task grpo
